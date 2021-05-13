60c8b9deda05690c3d86ca382b8b3fb9ca974288
AlekseiZhukov <alex__zhukov@mail.ru>
2021-05-13 11:37:31 +0300

    add eslint and prietter 2

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import css from './App.module.scss';
 
 const App = () => {
   console.log('####: Some log');
+  console.log('####: create for check git commit operation');
   return <div className={cn(css.header, css.color)}>Hi! This is App Component!</div>;
 };
 
