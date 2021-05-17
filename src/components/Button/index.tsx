c47670a389e802b909e5e16009e4682bea48bfe8
AlekseiZhukov <alex__zhukov@mail.ru>
2021-05-17 21:04:47 +0300

    fix Heading Button and Parallax components

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 import cn from 'classnames';
 import s from './Button.module.scss';
 
+type styleButtonValues = 'smallBlueButton' | 'smallGreenButton' | 'mainYellowButton' | 'mainButton';
+
 interface ButtonProps {
   onClick: (event: React.MouseEvent<HTMLButtonElement>) => void;
-  styleButton?: string;
+  styleButton?: styleButtonValues;
 }
 
 const Button: React.FC<ButtonProps> = ({ children, onClick, styleButton = 'mainButton' }) => {
