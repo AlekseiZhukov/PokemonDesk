5f3b6eaf993e3ab839fe0f663319047e0aeb27c1
AlekseiZhukov <alex__zhukov@mail.ru>
2021-05-29 12:39:59 +0300

    add tests for getWithParamsConfig function

--- src/utils/getUrlWithParamsConfig.test.ts
+++ src/utils/getUrlWithParamsConfig.test.ts
@@ -45,18 +45,84 @@ describe('getUrlWithParamsConfig', () => {
       body: {},
     });
   });
+  test('Должна принимать два аргумента "createPokemon" и пустой объект, на выходе получить объект с полями method, объектом uri с полями pathname, protocol, host и пустым query и поле body c пустым объектом', () => {
+    const url = getUrlWithParamsConfig('createPokemon', {});
 
-  /* test('принимает аргумента объект и объект', () => {
-        const url = getUrlWithParamsConfig({path: '/api/v1/pokemon/25'}, {id: 25});
-        expect(url).toEqual({
-            method: 'GET',
-            uri: {
-                protocol: 'http',
-                host: 'zar.hosthot.ru',
-                pathname: '/api/v1/pokemon/25',
-                query: {}
-            },
-            body: {}
-        })
-    }) */
+    expect(url).toEqual({
+      method: 'POST',
+      uri: {
+        protocol: 'http',
+        host: 'zar.hosthot.ru',
+        pathname: '/api/v1/pokemon/create',
+        query: {},
+      },
+      body: {},
+    });
+  });
+
+  test("Должна принимать два аргумента \"createPokemon\" и объект c полями {name: 'Azhukoff', weight: 80}, на выходе получить объект с полями method, объектом uri с полями pathname, protocol, host и пустым query и поле body c  объектом {name: 'Azhukoff', weight: 80}", () => {
+    const url = getUrlWithParamsConfig('createPokemon', { name: 'Azhukoff', weight: 80 });
+
+    expect(url).toEqual({
+      method: 'POST',
+      uri: {
+        protocol: 'http',
+        host: 'zar.hosthot.ru',
+        pathname: '/api/v1/pokemon/create',
+        query: {},
+      },
+      body: {
+        name: 'Azhukoff',
+        weight: 80,
+      },
+    });
+  });
+
+  test("Должна принимать два аргумента \"createPokemon\" и объект c полями {name: 'Azhukoff', weight: 80}, на выходе получить объект с полями method, объектом uri с полями pathname, protocol, host и пустым query и поле body c  объектом {name: 'Azhukoff', weight: 80}", () => {
+    const url = getUrlWithParamsConfig('createPokemon', { name: 'Azhukoff', weight: 80 });
+
+    expect(url).toEqual({
+      method: 'POST',
+      uri: {
+        protocol: 'http',
+        host: 'zar.hosthot.ru',
+        pathname: '/api/v1/pokemon/create',
+        query: {},
+      },
+      body: {
+        name: 'Azhukoff',
+        weight: 80,
+      },
+    });
+  });
+
+  test('Должна принимать два аргумента "updatePokemon" и { id: 25}, на выходе получить объект с полями method, объектом uri с полями pathname, protocol, host и пустым query и пустым body ', () => {
+    const url = getUrlWithParamsConfig('updatePokemon', { id: 25 });
+
+    expect(url).toEqual({
+      method: 'PATCH',
+      uri: {
+        protocol: 'http',
+        host: 'zar.hosthot.ru',
+        pathname: '/api/v1/pokemon/25',
+        query: {},
+      },
+      body: {},
+    });
+  });
+
+  test('Должна принимать два аргумента "deletePokemon" и { id: 25}, на выходе получить объект с полями method, объектом uri с полями pathname, protocol, host и пустым query и пустым body ', () => {
+    const url = getUrlWithParamsConfig('deletePokemon', { id: 25 });
+
+    expect(url).toEqual({
+      method: 'DELETE',
+      uri: {
+        protocol: 'http',
+        host: 'zar.hosthot.ru',
+        pathname: '/api/v1/pokemon/25/delete',
+        query: {},
+      },
+      body: {},
+    });
+  });
 });
